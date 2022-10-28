a8dc5eb127ebb4d0277c85f5bf519535e169cdd5
Ashwaq Aljanahi <94136081+ashwaqaljanahi2021@users.noreply.github.com>
2022-10-28 17:06:46 -0400

    Update Files.cpp
    How to write Files and add more to a File

--- Files.cpp
+++ Files.cpp
@@ -6,18 +6,25 @@ using namespace std;
 int main()
 {
 ofstream Out; // this declartion of object states the name that will be used to write the file
-
-
-
 Out.open("out.txt"); //this creates the file that we want to write in also it has to be in a string
-
+if(Out.is_open())
+{
+ 
 Out<<"Hello my name is Ashwaq!"<<endl; //Out acts like a cout, but it will be displayed in the file rather than the console! 
 
 Out.close( ); //It is important to close the files to avoid any bugs
 
-
-//This will print out in a separate file called out.txt: Hello my name is Ashwaq!
-
+}
+Out.open("out.txt",ios::app); //app will add to our file that we created
+if(Out.is_open())
+{
+  
+  Out<<"Nice to meet you!!"<<endl;
+  Out.close();
+  
+  
+  
+}
 
 
 
